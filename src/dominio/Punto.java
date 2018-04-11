dc18041e581639cb4c342fbde061dfc37878a975
natan <natan@DESKTOP-NATA>
2018-04-11 20:28:18 -0300

    Subiendo Punto1 del TP03 Programación Visual

---
+++ src/dominio/Punto.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dominio;
+
+/**
+ *
+ * @author natan
+ */
+public class Punto {
+    private int x;
+    private int y;
+    
+    //constructor vacío
+    public Punto() {
+    }
+    
+    //constructor con parámetros
+    public Punto(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+   
+    //Métodos accesores
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+   
+    public double calcularDistanciaEntrePuntos(Punto otroPunto){
+        return Math.sqrt(Math.pow(otroPunto.getX()-x, 2)+Math.pow(otroPunto.getY()-y, 2));
+    }
+    
+    
+    
+}
---
+++ src/main/Principal.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import dominio.Punto;
+
+/**
+ *
+ * @author natan
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Punto punto1 = new Punto(1, 2);
+        Punto punto2 = new Punto(3, 4);
+        
+        double resultado = punto1.calcularDistanciaEntrePuntos(punto2);
+        System.out.println("La distancia entre los puntos P1("+punto1.getX()+","+punto1.getY()+") y P2("+punto2.getX()+","+punto2.getY()+") es: "+resultado);
+    }
+    
+}
