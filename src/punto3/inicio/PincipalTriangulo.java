d0db7215e1a810c94a58bf114ec9c2249646c434
Lore <Lore@DESKTOP-CGP1K6U>
2018-04-13 12:11:44 -0300

    usuario loremirok

--- src/punto3/inicio/PincipalTriangulo.java
+++ src/punto3/inicio/PincipalTriangulo.java
@@ -3,9 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package main;
+package punto3.inicio;
 
-import dominio.Triangulo;
+import punto3.dominio.Triangulo;
 import java.util.Scanner;
 
 /**
--- src/punto4/inicio/PrincipalArregloDeNumero.java
+++ src/punto4/inicio/PrincipalArregloDeNumero.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package main;
+package punto4.inicio;
 
 import dominio.ArregloDeNumero;
 import java.util.Scanner;
