b56cdced08b0621d3144b4185412095431d762d4
SONY <SONY@VAIO>
2018-04-16 10:55:45 -0300

    agrege una clase Auto con sus atributos patente,marca,etc , una clase arreglo<deAuto con un arreglo de 4, y una clase main PrincipalAuto con una lista sobrecargada y muestrola lista

---
+++ src/Punto8/util/PrincipalAuto.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Punto8.util;
+
+import Punto8.dominio.ArregloDeAuto;
+import Punto8.dominio.Auto;
+import java.util.Scanner;
+
+/**
+ *
+ * @author SONY
+ */
+public class PrincipalAuto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      //  Auto auto = new Auto();
+        Scanner scanner=new Scanner(System.in);
+        Auto toyota = new Auto("jdsh", "toyota", "mediano", "verde", "gas");
+        Auto fitito = new Auto("kehs", "fitito", "grannde", "rojo", "nafta");
+        Auto chebrolet = new Auto("ehwd", "chebrolet", "mediano", "blanco", "nafta");
+
+        ArregloDeAuto arregloDeAutos = new ArregloDeAuto(3);
+        arregloDeAutos.agregarAuto(toyota, 0);
+        arregloDeAutos.agregarAuto(toyota, 1);
+        arregloDeAutos.agregarAuto(chebrolet, 2);
+
+        arregloDeAutos.mostrarArreglo();
+        System.out.println("Elija una posicin amodifica: ");
+        int  posicion=scanner.nextInt();
+        arregloDeAutos.modificarAuto(posicion);
+
+    }
+
+}
